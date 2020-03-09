44c4173800f39008e751069e7b5a83ac4200f12c
chengning-zhang <31712148+chengning-zhang@users.noreply.github.com>
2020-03-09 00:59:34 -0500

    Create WNB.py

---
+++ PU_Bayesian_classifiers/WNB.py
@@ -0,0 +1,108 @@
+class WNB(Bayes_net_PU):
+  name = "WNB"
+  def __init__(self,alpha = 1):
+    self.alpha = alpha
+
+  def fit(self,X_L, X_u, pri, M = None, case_control = True, model_class = LogisticRegression,  **kwargs):
+    """ Implementation of a fitting function.
+        Get fitted model that predict p(s=1|x), not related to sampling scenario
+        Parameters
+        ----------
+        X_l : {array-like, sparse matrix}, shape (n_samples, n_features)
+            The training input positive labeled samples.
+        X_u : {array-like, sparse matrix}, shape (n_samples, n_features)
+            The training input unlabeled samples.
+        pri : scalar
+            The prevalence p(y=1)
+        M : None, should not be used.
+            contact matrix.
+        case_control : Bool
+            Case control scenario or single-training data scenario, only change c_hat
+            Other part are same in both scenario.
+        model_class : a sklearn estimator, preferred logistic regression 
+                      since it gives calibrated proba, predict p(s=1|x)
+        **kwargs : 
+            extra parameters for model_class
+
+        Returns self
+        -------
+        self
+    """
+    X_L = check_array(X_L)
+    X_u = check_array(X_u)
+    if X_L.shape[1] != X_u.shape[1]:
+      raise ValueError('labeled data and unlabeled data have different number of features ')
+    n_L,p = X_L.shape
+    # encode categorical features
+    X = np.concatenate((X_L,X_u), axis = 0)
+    enc = preprocessing.OneHotEncoder(drop='first').fit(X)
+    X = enc.transform(X).toarray()
+    # X = pd.DataFrame(X).astype('category') # convert to categorical, for logistic regression to work. Does not work in general, has to encode, but different proba
+    y = np.concatenate( (np.repeat('1',X_L.shape[0] ), np.repeat('0',X_u.shape[0]) ),axis = 0)
+    # fit model g(x) = p(s=1|x)
+    model = model_class(**kwargs)
+    model.fit(X,y)
+    # estimate p(s=1)
+    p_s_1 = X_L.shape[0]/(X_L.shape[0]+X_u.shape[0])
+    # estimate c
+    if case_control:
+      c = p_s_1/(pri*(1-p_s_1) + p_s_1)
+    else: 
+      c = p_s_1/pri
+    # estimate w(x)
+    # w_L = np.repeat(1,X_L.shape[0])
+    inx = list(model.classes_ ).index('1')
+    g_U = model.predict_proba( X[n_L:] )[:,inx] # let us assume it is already calibrated ,it that already calibrated? 
+    w_U = ((1-c)/c) * (g_U/(1-g_U)) # maybe need to normalize
+    w_U = w_U - min(w_U) # make non-negative
+    w_U = w_U / max(w_U) # 0-1
+    # learning the coef_, p(xij|1), p(xij|0)
+    # extreme case: w_U correctly weight positive 1 and negative 0 in U, originally p(xij|1) = N_L(xij)/N_L, 
+    # List_count_1 = {} 
+    List_prob_1 = {} # {x0:{'1': p(x0 =1|y=1), '2': p(x0 =2|y=1), 'else': }, x1:{},   ... x7:{} }
+    #
+    List_prob_0 = {} # P(xi = j|c=0)
+    for i in range(p):
+      x_i_L_counter = Counter(X_L[:,i])
+      x_i_values = list(set(X_L[:,i]).union(set(X_u[:,i])))
+      # X_u positive weight counter, X_u negative weight counter
+      X_i_U_1_counter = {val: w_U[X_u[:,i] == val].sum() for val in x_i_values}
+      X_i_U_0_counter = {val: (1-w_U)[X_u[:,i] == val].sum() for val in x_i_values} # w_U has to be <1
+      # part 1, p(xi = j|1) = (N_L(xij) + sum_U_xij(w_U))/( n_L + sum(w_U))
+      List_prob_1[i] = {key: (self.alpha + x_i_L_counter[key] + X_i_U_1_counter[key])/ (self.alpha*len(x_i_values)  + n_L + w_U.sum() ) for key in x_i_values}
+      # part 2, p(xi = j|1)
+      List_prob_0[i] = {key: (self.alpha + X_i_U_0_counter[key])/ ((1-w_U).sum() + self.alpha*len(x_i_values) ) for key in x_i_values}
+    
+    self.is_fitted_ = True
+    self.case_control_ = case_control
+    self.List_prob_1_, self.List_prob_0_, self.c_, self.n_features_, self.w_U_, self.prevalence_ = List_prob_1, List_prob_0, c, p, w_U, pri
+    return self
+
+  def predict_proba(self,X): 
+    """
+        Return probability estimates for the test vector X. Usually it would be X_unlabeled
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+        Returns
+        -------
+        P(y=1|x) : array-like of shape (n_samples, )
+            Returns the probability of the samples for positive class in
+            the model. 
+    """
+    check_is_fitted(self)
+    X = check_array(X)
+    Prob_1 = []
+    for ins in X:
+      P1 = self.prevalence_ # don't need copy, immutable
+      P0 = 1 - P1
+      for i in range(self.n_features_):
+        P1 = P1 * (self.List_prob_1_[i][ins[i]]) 
+        P0 = P0 * (self.List_prob_0_[i][ins[i]]) 
+        # normalize proba
+      P = P1 + P0
+      P1 = P1/P; P0 = P0/P
+      Prob_1.append(P1)
+
+    Prob_1 = np.array(Prob_1) # for shap 
+    return Prob_1
